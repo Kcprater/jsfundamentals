87b51f8a3226e643220b20bae7442e6608f5d428
kcprater <kristopherprater@gmail.com>
2020-01-28 16:19:38 -0500

    new

---
+++ 5-expressions&Operators/01-assignmentOperators.js
@@ -0,0 +1,20 @@
+// ASSIGNMENT OPERATOR
+let r = 5;
+
+// ADDITION ASSIGNMENT
+r += 1; // r = r + 1
+
+// SUBTRACTION
+r -=1; // r= r - 1
+
+// MULTIPLICATION ASSIGNMENT
+r *= 1; // r = r * 1
+
+// DIVISION ASSIGNMENT
+r /= 1; // r = r / 1
+
+// REMAINDER (or modules) ASSIGNMENT
+r %= 1 // r = r % 1
+
+// EXPONENTIAL ASSIGNMENT
+r **= 1; // r = r ** 1
